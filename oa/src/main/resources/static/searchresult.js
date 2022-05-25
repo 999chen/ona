878e5220ed7b30063f0ccd12839322e809875107
Ckm <2043211535@qq.com>
2022-05-25 20:18:14 +0800

    init

--- oa/src/main/resources/static/searchresult.js
+++ oa/src/main/resources/static/searchresult.js
@@ -1,7 +1,7 @@
 window.onload=function () {
 // 1.将发送请求的题目信息封装在搜索框中
     var problem = window.location.search;
-    alert(problem);
+    // alert(problem);
     if (problem.indexOf("?"!=-1)){
         problem=problem.substr(problem.indexOf("=")+1);
     }
@@ -18,7 +18,6 @@ window.onload=function () {
         dataType: "json",
         // 请求成功,function形参data用来接收从后台传过来的数据
         success: function (data) {
-            console.log(data[0].pushuser);
            $("#user1").text("");
            $("#user1").append(data[0].pushuser);
            $("#title1").text("");
@@ -33,19 +32,52 @@ window.onload=function () {
            $("#star").append(data[0].star);
            $("#pushtime").text("");
            $("#pushtime").append(data[0].pushtime);
-           // $("#user1").html(data.pushuesr);
-           // $("#title1").html(data.title);
-           // $("#problemmessage").html(data.problem);
-           // $("#good1").html(data.good);
-           // $("#comment").html(data.comment);
-           // $("#star").html(data.star);
-           // $("#pushtime").html(data.pushtime);
-            alert("成功")
-
         },
         // 请求失败
         error: function () {
             alert("失败！")
         }
     });
+}
+
+
+
+
+function getData() {
+    var problem = $("#problem").val()
+    $.ajax({
+        // 请求地址
+        url: "http://localhost:8080/search",
+        // 数据传输方式
+        type: "get",
+        //从前台传后台的数据
+        data: {problem: problem},
+        // 从后台传到前台的数据类型
+        dataType: "json",
+        // 请求成功,function形参data用来接收从后台传过来的数据
+        success: function (data) {
+            cutval(data);
+        },
+        // 请求失败
+        error: function () {
+            alert("失败！")
+        }
+});
+}
+
+function cutval(data) {
+    $("#user1").text("");
+    $("#user1").append(data[0].pushuser);
+    $("#title1").text("");
+    $("#title1").append(data[0].title);
+    $("#problemmessage").text("");
+    $("#problemmessage").append(data[0].problem);
+    $("#good1").text("");
+    $("#good1").append(data[0].good);
+    $("#comment").text("");
+    $("#comment").append(data[0].comment);
+    $("#star").text("");
+    $("#star").append(data[0].star);
+    $("#pushtime").text("");
+    $("#pushtime").append(data[0].pushtime);
 }
